a60c2b155e8a29de0ac77db45c562588294d4118
Prince <princepriyadarshi2017@gmail.com>
2023-12-16 01:45:45 +0530

    init

--- 0001_Two Sum/TwoSum.java
+++ 0001_Two Sum/TwoSum.java
@@ -1,10 +1,7 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 // You can return the answer in any order.
-
- 
 
 // Example 1:
 
@@ -22,32 +19,29 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-
-
-
-
-
 import java.util.*;
 
 public class TwoSum {
-	public int[] twoSum(int[] nums, int target) {
-		int[] ret = new int[2];
-		ret[0] = -1;
-		ret[1] = -1;
-		if(nums == null || nums.length <= 1) return ret;
-		
-        Map<Integer,Integer> cache = new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-        	if(cache.containsKey(target - nums[i])){
-        		ret[0] = cache.get(target - nums[i]);
-        		ret[1] = i;
-        		break;
-        	}else{
-        		if(!cache.containsKey(nums[i]))
-        			cache.put(nums[i], i);
-        	}
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complements = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer complementIndex = complements.get(nums[i]);
+            if (complementIndex != null) {
+                return new int[]{i, complementIndex};
+            }
+            complements.put(target - nums[i], i);
         }
-        
-        return ret;
+        return new int[2]; // Return a default array if no sum is found
+    }
+
+    public static void main(String[] args) {
+        TwoSum obj = new TwoSum();
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] result = obj.twoSum(nums, target);
+        System.out.println("Indices with the sum: " + Arrays.toString(result));
     }
 }
+
+
